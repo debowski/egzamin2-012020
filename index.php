c09633f5230e88e057b9e2275b1a6af50d3e88a1
piotrroae <72742501+piotrroae@users.noreply.github.com>
2020-11-23 12:38:49 +0100

    Html + css
    html + css

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Nasz sklep komputerowy</title>
+    <link rel="stylesheet" type="text/css" href="styl8.css">
+  </head>
+  <body>
+
+    <div class="menu">
+      <a href="index.php">Główna</a>
+      <a href="procesory.html">Procesory</a>
+      <a href="ram.html">RAM</a>
+      <a href="grafika.html">Grafika</a>
+    </div>
+
+    <div class="logo">
+      <h2>Podzespoły komputerowe</h2>
+    </div>
+
+    <div class="glowny">
+      <h1>Dzisiejsze promocje</h1>
+      <table>
+        <tr>
+          <th>NUMER</th>
+          <th>NAZWA PODZESPOŁU</th>
+          <th>OPIS</th>
+          <th>CENA</th>
+        </tr>
+        <tr>
+          <td>aaa</td>
+          <td>bbb</td>
+          <td>ccc</td>
+          <td>ddd</td>
+        </tr>
+      </table>
+
+    </div>
+
+      <div class="stopka">
+        <img src="scalak.jpg" alt="promocje na procesory">
+      </div>
+
+      <div class="stopka">
+        <h4>Nasz  Sklep Komputerowy</h4>
+        <p>Współpracujemy  z  hurtownią  <a href="http://www.edata.pl/">edata</a></p>
+      </div>
+
+      <div class="stopka">
+        <p>zadzwoń: 601 602 603</p>
+      </div>
+
+      <div class="stopka">
+        <p>Stronę wykonał: 123456781011</p>
+      </div>
+
+  </body>
+</html>
